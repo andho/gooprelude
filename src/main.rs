5d4aaf543e0301c7d9a0fc7d09dcca58cd5b4dce
Andho Mohamed <andhos@gmail.com>
2023-07-25 16:53:26 +0500

    feat: control the window present mode :sparkles:

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,26 @@ mod animator;
 mod field_of_view;
 mod scene;
 
-use bevy::prelude::*;
+use bevy::{prelude::*, diagnostic::{LogDiagnosticsPlugin, FrameTimeDiagnosticsPlugin}, window::{PresentMode, WindowTheme}};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use game::GamePlugin;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, GamePlugin))
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "GoO prelude".into(),
+                    present_mode: PresentMode::AutoVsync,
+                    window_theme: Some(WindowTheme::Dark),
+                    ..default()
+                }),
+                ..default()
+            }),
+            GamePlugin,
+            FrameTimeDiagnosticsPlugin,
+            LogDiagnosticsPlugin::default(),
+        ))
         .add_plugins(WorldInspectorPlugin::new())
         .run();
 }
